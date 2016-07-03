a5ae7867231bf274ce9fc11551797197546c3c50
hapic <hapic@163.com>
2016-07-03 15:10:06 +0800

    添加iservice接口包

---
+++ oa-iservice/src/main/java/com/el/oa/iservice/Test.java
@@ -0,0 +1,30 @@
+package com.el.oa.iservice;
+
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ *
+ * @User : Hapic
+ * @Date : 2016/7/3 15:03
+ */
+public class Test {
+}
--- oa-mapper/src/main/java/com/el/oa/mapper/Test.java
+++ oa-mapper/src/main/java/com/el/oa/mapper/Test.java
@@ -4,4 +4,7 @@ package com.el.oa.mapper;
  * Created by ShenQi on 16/7/1.
  */
 public class Test {
+    public static void main(String[] args) {
+
+    }
 }
