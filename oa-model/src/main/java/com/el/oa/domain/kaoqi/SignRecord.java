ca1808f64bd44f79d2238b6d924cad9db00f22d8
hapic <hapic@163.com>
2016-07-06 21:22:56 +0800

    提交转换对象代码

---
+++ oa-model/src/main/java/com/el/oa/domain/kaoqi/SignRecord.java
@@ -0,0 +1,97 @@
+package com.el.oa.domain.kaoqi;
+
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ *
+ * @User : Hapic
+ * @Date : 2016/7/6 20:41
+ */
+public class SignRecord {
+    private Integer signId;//打卡id
+    private Integer uid;//用户id
+    private String date;//打卡时间
+    private String target;//打卡目的
+    private String type;//打卡方式
+    private String machineNum;//机器编码
+
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getSignId() {
+        return signId;
+    }
+
+    public void setSignId(Integer signId) {
+        this.signId = signId;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMachineNum() {
+        return machineNum;
+    }
+
+    public void setMachineNum(String machineNum) {
+        this.machineNum = machineNum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "SignRecord{" +
+                "signId=" + signId +
+                ", uid=" + uid +
+                ", date='" + date + '\'' +
+                ", target='" + target + '\'' +
+                ", type='" + type + '\'' +
+                ", machineNum='" + machineNum + '\'' +
+                '}';
+    }
+}
